a8fa49c13fa8af59e5eb284f86afb8ec03c1c596
Kishan_Gupta <guptakishan428@gmail.com>
2024-03-20 18:31:43 +0530

    recursion

---
+++ recursion/subSeq.java
@@ -0,0 +1,15 @@
+public class subSeq {
+    public static void main(String[] args) {
+        subseq("", "abc");
+    }
+
+    static void subseq(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        subseq(p + ch, up.substring(1));
+        subseq(p, up.substring(1));
+    }
+}
