3e1aedd5d49157d080e39f76e5d082c847c7e751
Kishan_Gupta <guptakishan428@gmail.com>
2024-03-26 10:01:32 +0530

    backtracking

--- Bactracking/maze.java
+++ Bactracking/maze.java
@@ -1,6 +1,7 @@
  class maze {
     public static void main(String[] args) {
         System.out.println(count(3, 3));
+        path("", 3, 3);
     }
 
     public static int count(int row, int col) {
@@ -11,5 +12,18 @@
             int right = count(row, col - 1);
             return left + right;
         
+    }
+
+    public static void path(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            System.out.println(p);
+            return;
+        }
+        if (r > 1) {
+            path(p + 'R', r - 1, c);
+        }
+        if (c > 1) {
+            path(p + 'D', r, c - 1);
+        }
     }
 }
