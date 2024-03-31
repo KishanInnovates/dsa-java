d4294f4d2b6eed89ee5a16781b634690693bc813
Kishan_Gupta <guptakishan428@gmail.com>
2024-03-31 23:00:38 +0530

    string

--- Easy/tempCodeRunnerFile.java
+++
@@ -1,1 +0,0 @@
-System.out.println(Arrays.toString(array));
---
+++ String/LongestPalindromic.java
@@ -0,0 +1,34 @@
+public class LongestPalindromic {
+    public static void main(String[] args) {
+        String s = "babad";
+        System.out.println(longest(s));
+    }
+
+    public static String longest(String s) {
+        if (s == null || s.length() < 1)
+            return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAround(s, i, i);
+            int len2 = expandAround(s, i, i + 1);
+
+            int len = Math.max(len1, len2);
+
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public static int expandAround(String s, int left, int right) {
+        int L = left;
+        int R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+}
