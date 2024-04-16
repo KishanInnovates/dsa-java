d2eb7f9caafcfe71afb33c8359ed0b8d79382bac
Kishan_Gupta <guptakishan428@gmail.com>
2024-04-16 15:09:11 +0530

    linkedlist

---
+++ LinkedList/LL.java
@@ -0,0 +1,24 @@
+public class LL {
+    private Node head;
+    private Node tail;
+
+    private int size;
+
+    public LL() {
+        this.size = 0;
+    }
+
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
---
+++ LinkedList/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        
+    }
+}
