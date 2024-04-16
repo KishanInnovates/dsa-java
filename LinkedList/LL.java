3f69a77a7d10a1880a6b026defd99cd786eb1f98
Kishan_Gupta <guptakishan428@gmail.com>
2024-04-16 15:22:35 +0530

    ll

--- LinkedList/LL.java
+++ LinkedList/LL.java
@@ -8,6 +8,25 @@ public class LL {
         this.size = 0;
     }
 
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+
+        if (tail == null) {
+            tail = head;
+        }
+        size += 1;
+    }
+    public void display(){
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+
     private class Node{
         private int value;
         private Node next;
--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         LL list = new LL();
-        
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertFirst(5);
+        list.insertFirst(9);
+        list.insertFirst(44);
+        list.display();
     }
 }
