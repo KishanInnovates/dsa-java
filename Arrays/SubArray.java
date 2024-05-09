fd322626d753664de803c91727e7658e52ec860c
Kishan_Gupta <guptakishan428@gmail.com>
2024-05-09 19:15:29 +0530

    gfgmedium

--- Arrays/SubArray.java
+++ Arrays/SubArray.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class SubArray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4 ,5 , 6, 7};
+        int n = 5;
     }
 
     public static ArrayList<Integer> subArraySum(int[] arr, int s) {
