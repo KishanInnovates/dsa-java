4d5ff43a53ff9cd0f8d67a0cf3ea4907a8858f32
Kishan_Gupta <guptakishan428@gmail.com>
2024-05-31 17:48:23 +0530

    linkedKList

--- LinkedList/MergeTwoSortedList.java
+++ LinkedList/MergeTwoSortedList.java
@@ -43,10 +43,12 @@ class MergeTwoSortedList {
         head1.next = new Node(10);
         head1.next.next = new Node(15);
         head1.next.next.next = new Node(40);
+        head1.next.next.next.next = new Node(50);
         
          Node head2 = new Node(2);
         head2.next = new Node(3);
         head2.next.next = new Node(20);
+        head2.next.next.next = new Node(70);
 
         Node mergedHead1 = list.sortedMerge(head1, head2);
         System.out.print("Merged List 1: ");
