9558cd04ff1ae7a845b5d9915eb6ad183f0b35cb
Kishan-Gupta <141546704+KishanInnovates@users.noreply.github.com>
2024-06-27 22:44:11 +0530

    Update ipaddress.java

--- Day-3/ipaddress.java
+++ Day-3/ipaddress.java
@@ -2,6 +2,7 @@ class ipaddress {
     public static void main(String args[]){
         String ip = "189.14.15.15";
         String newip = ip.replace(".","[.]");
+//to print
         System.out.print(newip);
     }
     
