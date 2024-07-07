c3db4331af9cccec7c163142a8817bde43e55867
Kishan_Gupta <guptakishan428@gmail.com>
2024-07-07 22:32:08 +0530

    potdLeetcode

--- Easy/Pillow.java
+++ Easy/Pillow.java
@@ -1,6 +1,6 @@
 public class Pillow {
     public static void main(String[] args) {
-
+            //input
     }
     public static int passThePillow(int n, int time) {
         int cycleLength = 2 * ( n - 1);
