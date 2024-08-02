65236c05b4ae05986d1f094216573f62bcff3593
Kishan_Gupta <guptakishan428@gmail.com>
2024-08-02 22:23:37 +0530

    stackANDqueue

--- StackAndQueue/QueueUsingStack.java
+++ StackAndQueue/QueueUsingStack.java
@@ -15,4 +15,26 @@ class queueUsingStack {
         first = new Stack<>();
         second = new Stack<>();
     }
+
+    public void add(int item) {
+        first.push(item);
+    }
+
+    public int remove() {
+        while (!first.isEmpty()) {
+            second.push(first.pop());
+        }
+
+        int removed = second.pop();
+
+        while (!second.isEmpty()) {
+            first.push(second.pop());
+        }
+
+        return removed;
+    }
+
+    public boolean isEmpty(){
+        return first.isEmpty();
+    }
 }
