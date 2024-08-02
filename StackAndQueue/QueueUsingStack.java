568270f0752c8f66f6c8781321f098b8f8b5baec
Kishan_Gupta <guptakishan428@gmail.com>
2024-08-02 22:14:37 +0530

    stackANDqueue

---
+++ StackAndQueue/QueueUsingStack.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+public class QueueUsingStack {
+    public static void main(String[] args) {
+        
+    }
+
+}
+
+class queueUsingStack {
+    private Stack<Integer> first;
+    private Stack<Integer> second;
+
+    public queueUsingStack() {
+        first = new Stack<>();
+        second = new Stack<>();
+    }
+}
