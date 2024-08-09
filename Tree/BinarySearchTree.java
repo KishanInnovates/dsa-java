04a6c8a39e2b57bccb96f7de6671e17131acb6a2
Kishan_Gupta <guptakishan428@gmail.com>
2024-08-09 17:25:39 +0530

    BinarySearchTree

--- Tree/BinarySearchTree.java
+++ Tree/BinarySearchTree.java
@@ -30,4 +30,37 @@ public class BinarySearchTree {
     public boolean isEmpty() {
         return root == null;
     }
+
+    public void display() {
+        display(root, "Root Node: ");
+    }
+
+    private void display(Node node, String details) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(details + node.getValue());
+        display(node.left, "Left child of " + node.getValue() + " : ");
+        display(node.right, "Right child of " + node.getValue() + " : ");
+    }
+
+    public void insert(int value) {
+
+    }
+    
+    private Node insert(int value, Node node) {
+        if (node == null) {
+            node = new Node(value);
+            return node;
+        }
+        if (value < node.value) {
+            node.left = insert(value, node.left);
+        }
+        if (value > node.value) {
+            node.right = insert(value, node.right);
+        }
+
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+        return node;
+    }
 }
