8d7c3fc493830aae5aeaebff77706e2dfc75aa70
Kishan_Gupta <guptakishan428@gmail.com>
2024-08-09 17:08:49 +0530

    BinarySearchTree

---
+++ Tree/BinarySearchTree.java
@@ -0,0 +1,33 @@
+public class BinarySearchTree {
+    public class Node {
+        private int value;
+        private int height;
+        private Node left;
+        private Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    private Node root;
+
+    public BinarySearchTree() {
+
+    }
+
+    public int height(Node node) {
+        if (node == null) {
+            return -1;
+        }
+        return node.height;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+}
