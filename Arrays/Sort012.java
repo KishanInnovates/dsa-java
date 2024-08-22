6620d7f6170ed00c493beee1ac06fca7133df11d
Kishan_Gupta <guptakishan428@gmail.com>
2024-08-22 21:53:03 +0530

    leetcode

--- Arrays/Sort012.java
+++ Arrays/Sort012.java
@@ -1,15 +1,22 @@
 public class Sort012 {
     public static void main(String[] args) {
          int[] arr = {0, 2, 1, 2, 0};
-        int n = arr.length;
+         int n = arr.length;
+         int[] arr2 = { 2, 0, 2, 1, 1, 0 };
+        int n2 = arr2.length;
         
         sort(arr, n);
+        sort(arr2, n2);
         
         for (int i : arr) {
             System.out.print(i + " ");
         }
+        System.out.println();
+         for (int i : arr2) {
+            System.out.print(i + " ");
+        }
     }
-
+    //time complexity : O(N) space complexity : O(1)
     public static void sort(int[] arr, int n) {
         int count0 = 0, count1 = 0, count2 = 0;
 
