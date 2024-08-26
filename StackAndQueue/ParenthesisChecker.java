9194fff51ec168d80ee18e69b245748638e9ef18
Kishan_Gupta <guptakishan428@gmail.com>
2024-08-26 10:06:49 +0530

    ParenthesisChecker

---
+++ StackAndQueue/ParenthesisChecker.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+public class ParenthesisChecker {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isPAr(String x) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : x.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (ch == ')') {
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    }
+                }
+                if (ch == '}') {
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    }
+                }
+                if (ch == ']') {
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
