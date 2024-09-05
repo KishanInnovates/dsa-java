c77f5467c1c1a6d5f27200262d43aa210f8dd34b
Kishan_Gupta <guptakishan428@gmail.com>
2024-09-05 22:55:18 +0530

    leetcode-75

--- Leetcode-75/MaximumVowel.java
+++ Leetcode-75/MaximumVowel.java
@@ -2,5 +2,26 @@ public class MaximumVowel {
     public static void main(String[] args) {
         
     }
-    public static int maxVowels()
+
+    public static int maxVowels(String s, int k) {
+        int maxVowels = 0;
+        int currentVowels = 0;
+
+        for (int i = 0; i < k; i++) {
+            if (isVowel(s.charAt(i))) {
+                currentVowels++;
+            }
+        }
+        maxVowels = currentVowels;
+        for (int i = k; i < s.length(); i++) {
+            if (isVowel(s.charAt(i))) {
+                currentVowels++;
+            }
+            if (isVowel(s.charAt(i - k))) {
+                currentVowels--;
+            }
+            maxVowels = Math.max(maxVowels, currentVowels);
+        }
+        return maxVowels;
+    }
 }
