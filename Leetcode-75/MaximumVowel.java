749aa5ae6ad860b9a3374d85d353e60055b23424
Kishan_Gupta <guptakishan428@gmail.com>
2024-09-05 22:58:45 +0530

    leetcode-75

--- Leetcode-75/MaximumVowel.java
+++ Leetcode-75/MaximumVowel.java
@@ -1,6 +1,9 @@
 public class MaximumVowel {
     public static void main(String[] args) {
-        
+        String s = "abciiidef";
+        int k = 3;
+        int result = maxVowels(s, k);
+        System.out.print("The length of the substring of vowel in " + s + " = " + result );
     }
 
     public static int maxVowels(String s, int k) {
@@ -23,5 +26,9 @@ public class MaximumVowel {
             maxVowels = Math.max(maxVowels, currentVowels);
         }
         return maxVowels;
+    }
+
+    public static boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
     }
 }
