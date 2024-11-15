cc483f892ee1aba11bc12b2284c6abb8166e36a4
Kishan_Gupta <guptakishan428@gmail.com>
2024-11-15 12:57:24 +0530

    pattern questions

--- A2Z Sheet/Pattern1.java
+++ A2Z Sheet/Pattern1.java
@@ -1,6 +1,6 @@
 class Pattern1 {
     public static void main(String[] args){
-        pattern4(5);
+        pattern7(5);
 
     }
 
@@ -35,6 +35,39 @@ class Pattern1 {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern5(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern6(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern7(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n-i-1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j < 2 * i ; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= n - i - 1; j++) {
+                System.out.print(" ");
             }
             System.out.println();
         }
