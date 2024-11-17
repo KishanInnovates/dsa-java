3c616b12a2065233e484a8f5eca06a9e796a0a90
Kishan_Gupta <guptakishan428@gmail.com>
2024-11-17 12:38:01 +0530

    pattern questions

--- A2Z Sheet/Pattern1.java
+++ A2Z Sheet/Pattern1.java
@@ -1,6 +1,6 @@
 class Pattern1 {
     public static void main(String[] args) {
-        pattern10(5);
+        pattern11(4);
 
     }
 
@@ -104,9 +104,30 @@ class Pattern1 {
             int start = (i % 2 == 0) ? 0 : 1;
             for (int j = 1; j <= i; j++) {
                 System.out.print(start + " ");
-                start = 1 - start;
+                start = 1 - start; //to filp the 1 and 0
             }
             System.out.println();
+        }
+    }
+
+    public static void pattern11(int n) {
+        int spaces = 2 * (n - 1);
+        for (int i = 1; i <= n; i++) {
+            //numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            //spaces
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+
+            //numbers
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+            spaces -= 2;
         }
     }
 }
