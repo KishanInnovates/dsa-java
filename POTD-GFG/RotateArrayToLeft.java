4532627d9b5017ecfc567dea3ef61663b52c6916
Kishan_Gupta <guptakishan428@gmail.com>
2024-11-18 14:43:18 +0530

    gfg-potd

---
+++ POTD-GFG/RotateArrayToLeft.java
@@ -0,0 +1,33 @@
+public class RotateArrayToLeft {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int d = 2;
+        rotateLeft(arr, d);
+    }
+
+    public static void rotateLeft(int[] arr, int d) {
+        int n = arr.length;
+        d = d % n;
+
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, n - 1);
+        reverse(arr, 0, n - 1);
+        printArray(arr);
+    }
+
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
