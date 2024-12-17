d10898d6da022a74c5a80159e282b5ce64775313
Kishan_Gupta <guptakishan428@gmail.com>
2024-12-17 21:47:33 +0530

    160DaysOfCode

--- 160DaysOdCode/SearchPattern.java
+++ 160DaysOdCode/SearchPattern.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class SearchPattern {
     public static void main(String[] args) {
         String txt1 = "abcab", pat1 = "ab";
-        System.out.print(search(pat1, txt1));
+        System.out.print(search(pat1, txt1)); //ouput : [0, 3]
     }
     // https://www.geeksforgeeks.org/batch/gfg-160-problems/track/string-gfg-160/problem/search-pattern0205
     public static ArrayList<Integer> search(String pat, String txt) {
