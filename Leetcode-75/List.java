ff4473ed6e313fceca907e9a19c37c1e20bf6cb2
Kishan_Gupta <guptakishan428@gmail.com>
2024-12-18 16:56:09 +0530

    HackerRank

--- Leetcode-75/List.java
+++ Leetcode-75/List.java
@@ -1,4 +1,14 @@
 
 public class List<T> {
 
+    public Object get(int i) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
 }
