059f93226593310be720316c921818849558c4f7
Kishan_Gupta <guptakishan428@gmail.com>
2025-03-19 21:37:55 +0530

    gfg-linkedlist

---
+++ LinkedList/JoinTwoLL.java
@@ -0,0 +1,15 @@
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+    }
+}
+class JoinTwoLL{
+    public static void main(String[] args) {
+        
+    }
+    public static Node joinTwoLL(Node head1, Node head2){
+        
+    }
+}
