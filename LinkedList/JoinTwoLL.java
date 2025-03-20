2ad718767be04d3c35b132d8a50790a714b34d34
Kishan_Gupta <guptakishan428@gmail.com>
2025-03-20 05:44:58 +0530

    gfg

--- LinkedList/JoinTwoLL.java
+++ LinkedList/JoinTwoLL.java
@@ -7,9 +7,14 @@ class Node{
 }
 class JoinTwoLL{
     public static void main(String[] args) {
-        
+        //o(n)
     }
     public static Node joinTwoLL(Node head1, Node head2){
-        
+        Node current = head1;
+        while(current.next != null){
+            current = current.next;
+        }
+        current.next = head2;
+        return head1;
     }
 }
