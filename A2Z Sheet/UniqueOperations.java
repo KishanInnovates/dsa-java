3626ccc0f9a7e9ab9f3fe1fac6f4b7dcd97e6535
Kishan_Gupta <guptakishan428@gmail.com>
2025-04-09 22:27:03 +0530

    leetcode-potd

---
+++ A2Z Sheet/UniqueOperations.java
@@ -0,0 +1,15 @@
+public class UniqueOperations {
+    public static void main(String[] args) {
+
+    }
+
+    public static int minOperations(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (!seen.add(nums[i])) {
+                return (i + 1 + 2) / 3;
+            }
+        }
+        return 0;
+    }
+}
