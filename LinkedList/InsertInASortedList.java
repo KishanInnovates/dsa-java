5e9b996ecd9a110f939fe2cc4e8e94b2a95cb352
Kishan_Gupta <guptakishan428@gmail.com>
2025-04-19 18:12:39 +0530

    gfg-easy

---
+++ LinkedList/InsertInASortedList.java
@@ -0,0 +1,19 @@
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+    }
+}
+class InsertInASortedList{
+    public static void main(String[] args) {
+
+    }
+
+    public static Node sortedInsert(Node head, int key) {
+        Node newNode = new Node(key);
+        if(head == null || key < head.data){
+            new
+        }
+    }
+}
