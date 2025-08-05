7f29f5171a3e87fe5a3f52b2c8984029741bf0f8
KishanInnovates <guptakishan428@gmail.com>
2025-08-05 22:31:55 +0530

    5-08-2025

---
+++ POTD-GFG/PalindromSentence.java
@@ -0,0 +1,17 @@
+public class PalindromSentence {
+    public static void main(String[] args) {
+
+    }
+        public boolean isPalinSent(String s) {
+            int l = 0, r = s.length() - 1;
+            while (l < r) {
+                while (l < r && !Character.isLetterOrDigit(s.charAt(l)))
+                    l++;
+                while (l < r && !Character.isLetterOrDigit(s.charAt(r)))
+                    r--;
+                if (Character.toLowerCase(s.charAt(l++)) != Character.toLowerCase(s.charAt(r--)))
+                    return false;
+            }
+            return true;
+        }
+}
